13e2ef3412eab1a45ebb94229ac68fab1e2d9ed2
Alisher <alishsem@gmail.com>
2021-05-16 22:24:07 +0900

    added some information on the homepage

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,19 +2,26 @@
 
 import MenuComponent from "./MenuComponent"
 import { DISHES } from '../shared/dishes'
+import { COMMENTS } from '../shared/comments'
+import { LEADERS } from '../shared/leaders'
+import { PROMOTIONS } from '../shared/promotions'
 import { useState } from 'react'
 import DishdetailComponent from './DishdetailComponent'
 import HeaderComponent from './HeaderComponent'
 import FooterComponent from './FooterComponent'
 import HomeComponent from './HomeComponent'
 import { Switch, Route, Redirect} from 'react-router-dom'
-
+import ContactComponent from './ContactComponent'
 
 function MainComponent() {
     const [dishes, setDishes] = useState(DISHES)
+    const [comments, setComments] = useState(COMMENTS)
+    const [leaders, setLeaders] = useState(LEADERS)
+    const [promotions, setPromotions] = useState(PROMOTIONS)
+
     const HomePage = () => {
         return (
-            <HomeComponent/>
+            <HomeComponent dish={dishes.filter((dish) => dish.featured)[0]} promotion={promotions.filter((promotion) => promotion.featured)[0]} leader={leaders.filter((leader) => leader.featured)[0]}/>
         )
     }
 
@@ -25,6 +32,7 @@ function MainComponent() {
             <Switch>
                 <Route path="/home" component={HomePage}/>
                 <Route exact path="/menu" component={() => <MenuComponent dishes={dishes}/>}/>
+                <Route exact path="/contactus" component={ContactComponent}/>
                 <Redirect to="/home"/>
             </Switch>
             <FooterComponent/>
