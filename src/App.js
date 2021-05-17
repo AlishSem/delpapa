3f3244d7b1d18303ecd3a30b04653210b81f9a15
Alisher <alishsem@gmail.com>
2021-05-17 22:40:32 +0900

    transferred all of the states to the redux reducer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import MainComponent from "./components/MainComponent"
 import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {ConfigureStore} from './redux/configureStore'
 
-
+const store = ConfigureStore()
 
 function App() {
-
+  
   return (
+    <Provider store ={store}>
     <BrowserRouter>
     <div>
       <MainComponent/>
     </div>
     </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,35 +1,46 @@
 
 
 import MenuComponent from "./MenuComponent"
-import { DISHES } from '../shared/dishes'
-import { COMMENTS } from '../shared/comments'
-import { LEADERS } from '../shared/leaders'
-import { PROMOTIONS } from '../shared/promotions'
-import { useState } from 'react'
+import { useState, Component } from 'react'
 import DishdetailComponent from './DishdetailComponent'
 import HeaderComponent from './HeaderComponent'
 import FooterComponent from './FooterComponent'
 import HomeComponent from './HomeComponent'
-import { Switch, Route, Redirect} from 'react-router-dom'
+import { Switch, Route, Redirect, withRouter} from 'react-router-dom'
 import ContactComponent from './ContactComponent'
 import AboutComponent from './AboutComponent'
+import {connect} from 'react-redux'
 
-function MainComponent() {
-    const [dishes, setDishes] = useState(DISHES)
-    const [comments, setComments] = useState(COMMENTS)
-    const [leaders, setLeaders] = useState(LEADERS)
-    const [promotions, setPromotions] = useState(PROMOTIONS)
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        leaders: state.leaders,
+        promotions: state.promotions
+    }
+}
+
+
+class MainComponent extends Component {
+
+
+    constructor(props){
+        super(props)
+    }
+
+
+    render(){
 
     const HomePage = () => {
         return (
-            <HomeComponent dish={dishes.filter((dish) => dish.featured)[0]} promotion={promotions.filter((promotion) => promotion.featured)[0]} leader={leaders.filter((leader) => leader.featured)[0]}/>
+            <HomeComponent dish={this.props.dishes.filter((dish) => dish.featured)[0]} promotion={this.props.promotions.filter((promotion) => promotion.featured)[0]} leader={this.props.leaders.filter((leader) => leader.featured)[0]}/>
         )
     }
 
     const DishWithId = ({match}) => {
         return (
-            <DishdetailComponent dish={dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-                comments={comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+            <DishdetailComponent dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
         )
     }
 
@@ -39,15 +50,16 @@ function MainComponent() {
             <HeaderComponent/>
             <Switch>
                 <Route path="/home" component={HomePage}/>
-                <Route exact path="/menu" component={() => <MenuComponent dishes={dishes}/>}/>
+                <Route exact path="/menu" component={() => <MenuComponent dishes={this.props.dishes}/>}/>
                 <Route path = "/menu/:dishId" component={DishWithId}/>
                 <Route exact path="/contactus" component={ContactComponent}/>
-                <Route exact path="/aboutus" component={() => <AboutComponent leaders = {leaders}/>}/>
+                <Route exact path="/aboutus" component={() => <AboutComponent leaders={this.props.leaders}/>}/>
                 <Redirect to="/home"/>
             </Switch>
             <FooterComponent/>
         </div>
     );
 }
+}
 
-export default MainComponent;
+export default withRouter(connect(mapStateToProps)(MainComponent));
